3d98238940a6f4c0a29fe35143084bc8b370779e
Davud-Ponjevic <144032033+DavudPonjevic@users.noreply.github.com>
2023-11-27 11:12:39 +0100

    GridDemo and function

---
+++ GridDemo/GridDemo/GridDemo/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GridDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonRight_Click(object sender, RoutedEventArgs e)
+        {
+            // Ändere den Inhalt des zweiten Buttons, wenn der erste Button geklickt wird
+            buttonLeft.Content = "Der Rechte Btn wurde geklickt!";
+
+            buttonRight.Content = "Rechter Btn";
+
+        }
+
+        private void ButtonLeft_Click(object sender, RoutedEventArgs e)
+        {
+            // Ändere den Inhalt des ersten Buttons, wenn der zweite Button geklickt wird
+            buttonRight.Content = "Der linke Btn wurde geklickt!";
+
+            // Setze den Inhalt des zweiten Buttons zurück
+            buttonLeft.Content = "Linker Btn";
+        }
+
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+            // Setze den Inhalt beider Buttons zurück
+            buttonRight.Content = "Rechter Btn";
+            buttonLeft.Content = "Linker Btn";
+        }
+
+    }
+}
--- GridDemo/Task.md
+++ GridDemo/Task.md
@@ -1,1 +1,19 @@
-Grid Demo
+# Grid Demo
+
+## Inhalt
+- [Grid](#grid)
+- [GridSplitter](#gridsplitter)
+- [Live Demo](#live-demo)
+
+## Grid
+Das Grid ist ein Layout-Container in XAML (eXtensible Application Markup Language),
+einer Markup-Sprache, die häufig für die Definition von Benutzeroberflächen
+in WPF (Windows Presentation Foundation) und anderen XAML-basierten Technologien verwendet wird. 
+Ein Grid ermöglicht die Aufteilung des verfügbaren Bildschirmplatzes in Zeilen und Spalten,
+um die Positionierung von UI-Elementen zu steuern.
+
+## GridSplitter 
+Ein GridSplitter ist ein UI-Steuerelement, das in einem Grid verwendet wird, um die Größe von Zeilen oder Spalten dynamisch zu ändern. Benutzer können den GridSplitter ziehen, um die Größe der benachbarten Zeilen oder Spalten anzupassen.
+
+
+## Live Demo
